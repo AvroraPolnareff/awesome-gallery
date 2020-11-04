c84765ae1fb129645642b8f7947789b3018293fd
AvroraPolnareff <araintheskywith@gmail.com>
2020-11-04 15:01:06 +0300

    enhance theme with breakpoints

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,6 +3,8 @@ import 'styled-components'
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
+      white: string;
+      black: string;
       primary: string;
       secondary: string;
       borders: string;
--- src/themes.ts
+++ src/themes.ts
@@ -4,7 +4,9 @@ export const theme: DefaultTheme = {
   colors: {
     primary: "#303030",
     secondary: "#303030",
-    borders: "#303030"
+    borders: "#303030",
+    black: "#000000",
+    white: "#ffffff"
   },
   breakpoints: {
     sm: '576px',
