9148574fa8044433aba32f84c7123aa388d3e39f
AvroraPolnareff <araintheskywith@gmail.com>
2020-11-04 06:59:13 +0300

    add themes to app

---
+++ src/themes.ts
@@ -0,0 +1,9 @@
+import {DefaultTheme} from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: "#303030",
+    secondary: "#303030",
+    borders: "#303030"
+  }
+}
