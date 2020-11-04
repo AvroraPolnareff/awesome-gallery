724f1a5e3f4eb7342279e653eede2c59d817115d
AvroraPolnareff <araintheskywith@gmail.com>
2020-11-04 15:01:44 +0300

    delete default boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import styled from "styled-components";
+import {AlbumsGallery} from "./components/pages/AlbumsGallery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Header>
+
+      </Header>
+      <AlbumsGallery albums={}/>
+    </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+  height: 100vh;
+`
+
+const Header = styled.header`
+
+`
 
 export default App;
