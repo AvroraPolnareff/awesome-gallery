d5c7ba15d89f7a7838b5f7a152fb9267ce86c120
AvroraPolnareff <araintheskywith@gmail.com>
2020-11-04 07:00:13 +0300

    import normalize to app and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'normalize.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {theme} from "./themes";
+import {ThemeProvider} from "styled-components";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
